08604c881daed1fd080b2e79a404019ae13b0fa7
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-06 14:19:21 +0200

    feat: aggiunge la lista di certificati nel componente

---
+++ src/App/Certificati/Card.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Box,
+  Card as MaterialCard,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  content: {
+    paddingBottom: 16,
+    "&:last-child": {
+      paddingBottom: 16,
+    },
+  },
+  actions: {
+    padding: 16,
+    paddingTop: 0,
+  },
+  overline: {
+    marginBottom: 0,
+    lineHeight: 1.1,
+  },
+});
+
+const dateStr = (date) => {
+  const mesi = [
+    "Gennaio",
+    "Febbraio",
+    "Marzo",
+    "Aprile",
+    "Maggio",
+    "Giugno",
+    "Luglio",
+    "Agosto",
+    "Settembre",
+    "Ottobre",
+    "Novembre",
+    "Dicembre",
+  ];
+
+  const arrDate = date.split("-");
+  const mese = mesi[parseInt(arrDate[1])];
+
+  return `${mese} ${arrDate[0]}`;
+};
+
+export function Card({ dettagli: d, ...rest }) {
+  const classes = useStyles();
+
+  return (
+    <Box {...rest}>
+      <MaterialCard>
+        <CardContent className={classes.content}>
+          <Typography
+            variant="overline"
+            className={classes.overline}
+            color="textSecondary"
+            gutterBottom
+          >
+            {d.provider}
+          </Typography>
+          <Typography variant="h5" component="h2">
+            {d.name}
+          </Typography>
+          <Typography color="textSecondary">
+            Emissione: {dateStr(d.issuing)}{" "}
+            {d.expiration && ` - Scadenza: ${dateStr(d.issuing)}`}
+          </Typography>
+        </CardContent>
+        {d.certUrl && (
+          <CardActions className={classes.actions}>
+            <Button
+              variant="contained"
+              color="secondary"
+              size="small"
+              onClick={() => window.open(d.certUrl, "_blank")}
+            >
+              certificato
+            </Button>
+          </CardActions>
+        )}
+      </MaterialCard>
+    </Box>
+  );
+}
--- src/App/Certificati/index.jsx
+++ src/App/Certificati/index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Container, Typography, Box } from "@material-ui/core";
 import { AppHeader } from "src/Components";
+import { Card } from "./Card";
 
 export function Certificati(props) {
+  let { certificati } = useSelector((state) => state);
+
   return (
     <React.Fragment>
       <AppHeader>
@@ -14,7 +18,11 @@ export function Certificati(props) {
       </AppHeader>
 
       <Box pt={4}>
-        <Container maxWidth="md">lulu</Container>
+        <Container maxWidth="md">
+          {certificati.map((f, i) => (
+            <Card dettagli={f} key={i} mb={4} />
+          ))}
+        </Container>
       </Box>
     </React.Fragment>
   );
